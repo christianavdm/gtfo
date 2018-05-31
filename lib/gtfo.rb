cccfabcab316aa6853108d375ae52a8590535c49
Christiana Vandermale <christianavdm@gmail.com>
2018-05-31 23:29:16 +0000

    Started Scraper class

--- lib/gtfo.rb
+++ lib/gtfo.rb
@@ -1,5 +1,12 @@
 require "gtfo/version"
+require 'nokogiri'
+require 'open-uri'
+require 'pry'
 
 module Gtfo
-  # Your code goes here...
+  class Scraper 
+    def get_page(url)
+      Nokogiri::HTML(open("url"))
+    end
+  end
 end
