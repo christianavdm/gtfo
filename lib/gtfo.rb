c7f3d76be942d27fdcf6babda80e3bf018ac4bd9
Christiana Vandermale <christianavdm@gmail.com>
2018-06-01 21:04:30 +0000

    Started adding scraper method

--- lib/gtfo.rb
+++ lib/gtfo.rb
@@ -5,13 +5,20 @@ require 'pry'
 
 module Gtfo
   class Scraper 
+    @destinations = []
     def get_page(url)
-      doc = Nokogiri::HTML(open("http://www.travelandleisure.com/trip-ideas/best-places-to-travel-in-2018")
-      binding.pry
+      doc = Nokogiri::HTML(open("http://www.travelandleisure.com/trip-ideas/best-places-to-travel-in-2018"))
+      
+      doc.css(".media-body").each do |destination|
+        
+      end
+      
+      
     end
   end
 end
 
-# hbd-image-caption-pn
-
-
+# big (box of data) = doc.css(".media-body")
+# first = first one
+# Title = .css("h2").strip
+# description = .css("p").text 
