f2a2ae039f93149ffa5f206aabcdded1ecaeb659
Christiana Vandermale <christianavdm@gmail.com>
2018-06-01 21:15:43 +0000

    Moved scraper method to the Destination class; deleted Scraper class.

--- lib/gtfo.rb
+++ lib/gtfo.rb
@@ -4,21 +4,7 @@ require 'open-uri'
 require 'pry'
 
 module Gtfo
-  class Scraper 
-    @destinations = []
-    def get_page(url)
-      doc = Nokogiri::HTML(open("http://www.travelandleisure.com/trip-ideas/best-places-to-travel-in-2018"))
-      
-      doc.css(".media-body").each do |destination|
-        new_destination = Destination.new 
-        new_destination.name = destination.css("h2").text.strip
-        new_destination.description = destination.css("p").text
-      end
-      
-      
-    end
-  end
-  
+
   class Destination 
     attr_accessor :name, :description
     
@@ -32,10 +18,16 @@ module Gtfo
       @@all 
     end
     
+    def destinations_from_page(url)
+      doc = Nokogiri::HTML(open("http://www.travelandleisure.com/trip-ideas/best-places-to-travel-in-2018"))
+      
+      doc.css(".media-body").each do |destination|
+        new_destination = Destination.new 
+        new_destination.name = destination.css("h2").text.strip
+        new_destination.description = destination.css("p").text
+      end
+    end
+    
   end
 end
 
-# big (box of data) = doc.css(".media-body")
-# first = first one
-# Title = .css("h2").strip
-# description = .css("p").text 
