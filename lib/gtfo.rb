0d9ca51ce4014eb057e731e32b6b809a10daf437
Christiana Vandermale <christianavdm@gmail.com>
2018-06-01 21:27:33 +0000

    Edited destinations_from_page method to delete the title object.

--- lib/gtfo.rb
+++ lib/gtfo.rb
@@ -18,16 +18,18 @@ module Gtfo
       @@all 
     end
     
-    def destinations_from_page(url)
+    def self.destinations_from_page
       doc = Nokogiri::HTML(open("http://www.travelandleisure.com/trip-ideas/best-places-to-travel-in-2018"))
       
       doc.css(".media-body").each do |destination|
         new_destination = Destination.new 
         new_destination.name = destination.css("h2").text.strip
         new_destination.description = destination.css("p").text
+        if new_destination.name == "The 50 Best Places to Travel in 2018"
+          Destination.all.delete(new_destination)
+          binding.pry
       end
     end
-    
   end
 end
 
