6cba1078be9acf10a9d5d265d7a6bd2f2d898bf9
Christiana Vandermale <christianavdm@gmail.com>
2018-06-02 14:55:58 +0000

    Added the correct required files to gtfo.rb

--- lib/gtfo.rb
+++ lib/gtfo.rb
@@ -1,4 +1,10 @@
-require "gtfo/version"
 require 'nokogiri'
 require 'open-uri'
 require 'pry'
+
+require_relative 'gtfo/cli'
+require_relative 'gtfo/destination'
+require_relative 'gtfo/version'
+
+module Gtfo 
+end
